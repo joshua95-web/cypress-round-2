40d4ebda7069eb4550d795887574cc13984a83c4
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-10-12 09:22:04 +0100

    updated cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "cypress";
+/*import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
@@ -12,6 +12,27 @@ export default defineConfig({
     devServer: {
       framework: "next",
       bundler: "webpack",
+    },
+  },
+});*/
+
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "tyhfus",
+  component: {
+    specPattern: "app/**/*.cy.{js,jsx,ts,tsx}",
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+  e2e: {
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:3000/",
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
