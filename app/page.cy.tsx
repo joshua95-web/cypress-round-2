896ad631ea3202904e1d8a66687d3b7f695428f9
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-10-12 11:55:19 +0100

    created a test under /app/page.cy.tsx

---
+++ app/page.cy.tsx
@@ -0,0 +1,13 @@
+describe("Calculator", () => {
+  it("renders a calculator", () => {
+    cy.visit("../app/page");
+    // check if all components are rendered
+    cy.get("result").should("exist");
+    cy.get("num1").should("exist");
+    cy.get("num2").should("exist");
+    cy.get("add").should("exist");
+    cy.get("subtract").should("exist");
+    cy.get("multiply").should("exist");
+    cy.get("divide").should("exist");
+  });
+});
